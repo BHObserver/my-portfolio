ce474012d2d2b49241c59843fbc0a8a59bf7cf10
BHObserver <BHObserver0@gmail.com>
2023-07-10 02:03:50 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,25 @@
 const navBtn = document.querySelector('.nav-btn');
 const header = document.querySelector('.header-desktop');
 const cross = document.querySelector('.cross');
+const navMobile = document.querySelector('.nav-mobile');
+const navItems = document.querySelectorAll('.header-desktop li');
 
 navBtn.addEventListener('click', () => {
   header.classList.add('mobile-menu');
-  header.innerHTML = '<div class="cross"></div>';
+  cross.style.display = 'block';
+  navMobile.style.display = 'block';
 });
 
 cross.addEventListener('click', () => {
   header.classList.remove('mobile-menu');
+  cross.style.display = 'none';
+  navMobile.style.display = 'none';
+});
+
+navItems.forEach((element) => {
+  element.addEventListener('click', () => {
+    header.classList.remove('mobile-menu');
+    cross.style.display = 'none';
+    navMobile.style.display = 'none';
+  });
 });
