5cae63cebd62d2ecc780c1b3ff71bdbfa5f4b475
BHObserver <BHObserver0@gmail.com>
2023-07-10 00:40:53 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-const navBtn = document.querySelector('.nav-btn')
-navBtn.addEventListener("click", function() {
-    
-})
+/* eslint-disable linebreak-style */
+const navBtn = document.querySelector('.nav-btn');
+const header = document.querySelector('.header-desktop');
+const cross = document.querySelector('.cross');
+
+navBtn.addEventListener('click', () => {
+  header.classList.add('mobile-menu');
+  header.innerHTML = '<div class="cross"></div>';
+});
+
+cross.addEventListener('click', () => {
+  header.classList.remove('mobile-menu');
+});
