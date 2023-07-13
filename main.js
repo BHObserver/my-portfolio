2fcd646f2b0f07d4c5a1788a1b636b4249d6621d
BHObserver <BHObserver0@gmail.com>
2023-07-13 11:10:35 +0600

    Update main.js

--- main.js
+++ main.js
@@ -29,15 +29,32 @@ navItems.forEach((element) => {
 
 /* Add pop up */
 const seeProjectBtn = document.querySelectorAll('.see-project-btn');
+const popUp = document.querySelector('.blur-bg');
+const cancelMobile = document.querySelector('.close-pop-up-mobile');
+const cancelDesktop = document.querySelector('.close-pop-up');
 const project = [
   {
     name: 'Keeping track of hundreds of components',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    image: '<img src="assets/Snapshoot Portfolio.jpg" class="img-pop-up" alt="portfolio-image">'
-  }
-]
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    image: '<img src="assets/Snapshoot Portfolio.jpg" class="img-pop-up" alt="portfolio-image">',
+  },
+];
 seeProjectBtn.forEach((e) => {
   e.addEventListener('click', () => {
-    
+    popUp.classList.toggle('hide');
   });
+});
+
+cancelDesktop.addEventListener('click', () => {
+  popUp.classList.add('hide');
+});
+
+cancelMobile.addEventListener('click', () => {
+  popUp.classList.add('hide');
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    popUp.classList.add('hide');
+  }
 });
