0e900eb6053d974540bd0b10f1c08dd357baeeee
maserekafelex <maserekafelex1@gmail.com>
2023-07-24 11:01:52 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+const navBtn = document.querySelector('.nav-btn');
+const header = document.querySelector('.header-desktop');
+const cross = document.querySelector('.cross');
+const navMobile = document.querySelector('.nav-mobile');
+const navItems = document.querySelectorAll('.header-desktop li');
