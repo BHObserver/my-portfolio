6c2656a0b732a3078ac9e46f1525e10f8c0ce02f
maserekafelex <maserekafelex1@gmail.com>
2023-07-24 11:02:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const header = document.querySelector('.header-desktop');
 const cross = document.querySelector('.cross');
 const navMobile = document.querySelector('.nav-mobile');
 const navItems = document.querySelectorAll('.header-desktop li');
+
+navBtn.addEventListener('click', () => {
+  header.classList.add('mobile-menu');
+  cross.style.display = 'block';
+  navMobile.style.display = 'block';
+});
+
